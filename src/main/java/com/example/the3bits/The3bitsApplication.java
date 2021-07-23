2b5446d970e2f7ac46869f24237775181fc673e0
davitmarg <davitmarg9@gmail.com>
2021-07-23 21:11:46 +0400

    Initial commit

---
+++ src/main/java/com/example/the3bits/The3bitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.the3bits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class The3bitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(The3bitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/the3bits/The3bitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the3bits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class The3bitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
