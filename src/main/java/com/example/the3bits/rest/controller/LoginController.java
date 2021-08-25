c094a99ee82c74dfd07c6f32097f0564fe7354b2
davitmarg <davitmarg9@gmail.com>
2021-08-25 19:38:04 +0400

    Login Controller

--- src/main/java/com/example/the3bits/rest/controller/LoginController.java
+++ src/main/java/com/example/the3bits/rest/controller/LoginController.java
@@ -5,12 +5,10 @@ import com.example.the3bits.rest.facade.user.model.DefaultUserRegistrationReques
 import com.example.the3bits.rest.facade.user.model.UserResponseModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class LoginController {
 
     private final UserFacade userFacade;
