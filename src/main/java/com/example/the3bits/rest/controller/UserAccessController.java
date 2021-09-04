265d78080e5c0aa2ea6355beb1867819e91a06bf
ASUS <heghine.aghajanyan.gk@gmail.com>
2021-09-04 10:20:39 +0400

    Some changes

--- src/main/java/com/example/the3bits/rest/controller/UserAccessController.java
+++ src/main/java/com/example/the3bits/rest/controller/UserAccessController.java
@@ -4,16 +4,20 @@ import com.example.the3bits.rest.facade.house.HouseFacade;
 import com.example.the3bits.rest.facade.house.model.HouseRequestModel;
 import com.example.the3bits.rest.facade.house.model.HouseResponseModel;
 import com.example.the3bits.rest.facade.rental.RentalFacade;
+import com.example.the3bits.rest.facade.rental.model.RentalRequestModel;
+import com.example.the3bits.rest.facade.rental.model.RentalResponseModel;
 import com.example.the3bits.rest.facade.user.UserFacade;
 import com.example.the3bits.rest.facade.user.model.UserResponseModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class UserAccessController {
 
     private final HouseFacade houseFacade;
@@ -34,4 +38,11 @@ public class UserAccessController {
         return ResponseEntity.ok(houseFacade.add(request));
     }
 
+    @PostMapping("/addAnnouncement")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    public ResponseEntity<RentalResponseModel> login(Authentication authentication, @RequestBody RentalRequestModel request) {
+        Long userId = userFacade.getIdByAuthentication(authentication);
+        request.setUserId(userId);
+        return ResponseEntity.ok(rentalFacade.add(request));
+    }
 }
